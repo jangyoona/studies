2b35e2a3263286e34373d2f3289e539d6a08a1cc
jangyoona <olozg@naver.com>
2026-06-30 14:21:53 +0900

    python: 클래스변수와 인스턴스변수2

--- python/클래스변수_인스턴스변수.py
+++ python/클래스변수_인스턴스변수.py
@@ -1,3 +1,6 @@
+# python에서 java나 c++처럼 강제되는 private 접근 제어자가 없다.
+# 따라서, 간단히 클래스 변수에 접근이 가능하다.
+
 class Calc:
     result = 0
     def setdata(self, fir, sec):
@@ -24,4 +27,14 @@ b.add()
 b.getdata()
 
 Calc.result += 1
-print(Calc.result)
+print(f'Calc.result={Calc.result}')
+#Calc.result = 3
+
+# t) 인스턴스에서 속성을 찾을 때 존재하지 않을 경우
+#    상위로 올라가 클래스 변수를 찾아 반환한다.
+print(f'a.result 삭제 전 result={a.result}')
+print(f'b.result 삭제 전 result={b.result}')
+del(a.result)
+del(b.result)
+print(f'a.result 삭제 후 result={a.result}')
+print(f'b.result 삭제 후 result={b.result}')
