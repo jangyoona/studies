612461a4a6ffc065c237d0169fb42a1c0f9d4926
jangyoona <olozg@naver.com>
2026-07-02 17:29:45 +0900

    chore: gitignore 추가

---
+++ c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// MSYS2 UCRT64 터미널에서
+// cd /d/github_repository/studies/c
+// gcc main.c -o main.exe
+
+int main(void) {
+    printf("Hello c world~\n");
+
+    return 0;
+}
--- python/test.py
+++ python/test.py
@@ -1,1 +1,13 @@
 # live
+
+def f(a):
+    m = [[x] for x in a]
+    print(m)
+    b = m[:]
+    print(b[1])
+    for i in range(len(b) - 1):
+        b[i+1] += b[i]
+    return sum(len(x) for x in m)
+# print(f([1, 2, 3, 4]))
+
+f([1, 2, 3, 4])
